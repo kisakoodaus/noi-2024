8df3bdd939aa5e7d1ec1bc00ced7c669cf7d11c7
Antti Laaksonen <ahslaaks@cs.helsinki.fi>
2024-02-15 19:38:55 +0200

    Add checker

--- juggling/checker.cpp
+++ juggling/checker.cpp
@@ -6,8 +6,35 @@
 
 int main()
 {
-    // TODO: Write the checker
-    // int n = read_int(1, 1e6);
+    int t = read_int(1,100);
 
-    end();
+    bool s1 = true;
+    bool s2 = true;
+    bool s3 = true;
+    bool s4 = true;
+
+    for (int i = 1; i <= t; i++) {
+        int n = read_int(1,100);
+        auto v = read_ints(n,1,n);
+
+        if (n > 10) s1 = false;
+        if (n > 20) s2 = false;
+
+        set<int> z;
+        for (auto x : v) {
+            if (z.count(x)) s3 = false;
+            z.insert(x);
+        }
+
+        for (auto x : v) if (x > 5) s4 = false;
+    }
+
+    vector<int> groups;
+    if (s1) groups.push_back(1);
+    if (s2) groups.push_back(2);
+    if (s3) groups.push_back(3);
+    if (s4) groups.push_back(4);
+    groups.push_back(5);
+
+    end_groups(groups);
 }
